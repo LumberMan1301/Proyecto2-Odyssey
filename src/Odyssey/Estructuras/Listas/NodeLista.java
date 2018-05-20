71ed37201681d90f37e1e078163f7d0a9712ec51
karlami <karla.rivera24@gmail.com>
2018-05-20 06:27:19 -0600

    06.27
    JavaDoc

--- src/Odyssey/Estructuras/Listas/NodeLista.java
+++ src/Odyssey/Estructuras/Listas/NodeLista.java
@@ -1,30 +1,34 @@
 package Odyssey.Estructuras.Listas;
 
 public class NodeLista<T> {
-	  private NodeLista<T> next;
-	  private T data;
-	 
-	  
-	  public NodeLista(T dato) {
-	    data = dato;
-	    next = null;
-	  }
-	  
-	  public T getData() {
-	    return data;
-	  }
-	  
-	  public void setData(T data) {
-	    this.data = data;
-	  }
-	  
-	  public NodeLista<T> getNext() {
-	    return next;
-	  }
-	  
-	  public void setNext(NodeLista<T> next) {
-	    this.next = next;
-	  }
-	  
+    private NodeLista<T> next;
+    private T data;
+
+/**
+ * Elemento basico para las listas, contiene el dato y referencia al siguiente y anterior
+ * segun lista lo requiera
+ * @param dato que contiene el objeto a guardar en la lista
+ */ 
+    public NodeLista(T dato) {
+      data = dato;
+      next = null;
+    }
+
+    public T getData() {
+      return data;
+    }
+
+    public void setData(T data) {
+      this.data = data;
+    }
+
+    public NodeLista<T> getNext() {
+      return next;
+    }
+
+    public void setNext(NodeLista<T> next) {
+      this.next = next;
+    }
+
 	  
 	}
